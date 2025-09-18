7b335340861cbf456e00b91ec3ee0f5e0a43cd82
SuJaLKuMaR-2511 <sujalkumar2511@gmail.com>
2025-09-18 12:20:34 +0530

    18 sept

--- LinkedHashSetExample.java
+++ LinkedHashSetExample.java
@@ -1,4 +1,6 @@
 import java.util.LinkedHashSet;
+import java.util.TreeSet;
+import java.util.HashSet;
 
 public class LinkedHashSetExample {
     public static void main(String[] args) {
@@ -6,14 +8,26 @@ public class LinkedHashSetExample {
         number.add(12);
         number.add(34);
         number.add(13);
-
         System.out.println("LinkedHashSet is : " + number);
+        
+        HashSet<Integer> hs= new HashSet<>();
+        hs.add(11);
+        hs.add(21);
+        hs.add(26);
+        System.out.println("HashSet is : " + hs);
 
+        
         LinkedHashSet<Integer> n1 = new LinkedHashSet<>();
         n1.add(11);
         n1.add(21);
         n1.add(26);
-
         System.out.println("New LinkedHashSet is : " + n1);
+
+        
+        TreeSet<Integer> ts = new TreeSet<>();
+        ts.add(11);
+        ts.add(21);
+        ts.add(26);
+        System.out.println("TreeSet is: "+ ts);
     }    
 }
